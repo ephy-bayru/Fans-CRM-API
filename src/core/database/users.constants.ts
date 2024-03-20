6144c9d2bfa1d6d4dbb056726ae1023b054fae6e
ephy-bayru <ephybayru@gmail.com>
2024-03-21 00:08:14 +0300

    feat(db-seed): add default users for initial database seeding

---
+++ src/core/database/users.constants.ts
@@ -0,0 +1,18 @@
+export const DEFAULT_USERS = [
+  {
+    firstName: 'Jane',
+    lastName: 'Doe',
+    userName: 'jane.doe',
+    email: 'jane.doe@example.com',
+    password: 'FansCRM@123!',
+    canLogin: true,
+  },
+  {
+    firstName: 'John',
+    lastName: 'Doe',
+    userName: 'john.doe',
+    email: 'john.doe@example.com',
+    password: 'FansCRM@123!',
+    canLogin: true,
+  },
+];
