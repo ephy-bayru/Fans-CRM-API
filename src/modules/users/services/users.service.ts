0c8e939a97e3d080e27b029bdaede30c78900758
ephy-bayru <ephybayru@gmail.com>
2024-03-20 23:42:13 +0300

    feat(users): enable lookup by username or email in findOne method

--- src/modules/users/services/users.service.ts
+++ src/modules/users/services/users.service.ts
@@ -1,8 +1,13 @@
-import { Injectable, BadRequestException } from '@nestjs/common';
+import {
+  Injectable,
+  BadRequestException,
+  NotFoundException,
+} from '@nestjs/common';
 import { AddUserDto } from '../dto/add-user.dto';
 import { User } from '../entities/user.entity';
 import { UsersRepository } from '../repositories/users-repository';
 import { UserResponseDto } from '../dto/user-response.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
@@ -12,8 +17,28 @@ export class UsersService {
     return this.usersRepository.read(id);
   }
 
+  async findOne(identifier: string): Promise<User | undefined> {
+    console.log('Message from UsersService');
+    const user = await this.usersRepository.findOne({
+      where: [{ userName: identifier }, { email: identifier }],
+    });
+
+    if (!user) {
+      throw new NotFoundException(
+        `User not found with identifier: ${identifier}`,
+      );
+    }
+
+    return user;
+  }
+
   // This method assumes that all validations, including uniqueness checks, have been passed
   async createUser(userData: AddUserDto): Promise<UserResponseDto> {
+    if (userData.canLogin) {
+      userData.password = await this.hashPassword(userData.password);
+    } else {
+      userData.password = null;
+    }
     const newUser = await this.usersRepository.create(userData);
     return this.mapToUserResponseDto(newUser);
   }
@@ -25,6 +50,7 @@ export class UsersService {
       lastName: user.lastName,
       userName: user.userName,
       email: user.email,
+      canLogin: user.canLogin,
     };
   }
 
@@ -43,5 +69,9 @@ export class UsersService {
       if (conflicts.email) errorMessage += 'Email is already taken.';
       throw new BadRequestException(errorMessage.trim());
     }
+  }
+
+  async hashPassword(password: string): Promise<string> {
+    return bcrypt.hash(password, 10);
   }
 }
